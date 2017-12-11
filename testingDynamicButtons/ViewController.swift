1df53af47ec1f4a509a575f070b9e0dafbab5b44
Luca Gaudino <nork18g@gmail.com>
2017-12-11 14:54:44 +0100

    Dynamic buttons example

--- testingDynamicButtons/ViewController.swift
+++ testingDynamicButtons/ViewController.swift
@@ -12,7 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+            
+        createButton(x: 100, y: 100, width: 100, height: 50)
+    }
+    
+    func createButton(x: Int, y: Int, width: Int, height: Int){
+        let button = UIButton(frame: CGRect(x: x, y: y, width: width, height: height))
+        button.backgroundColor = .green
+        button.setTitle("Test Button", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.setTitleColor(.black, for: .highlighted)
+        
+        self.view.addSubview(button)
+    }
+    
+    func createButtonWall(buttonNumber: Int){
+        for index in 0...buttonNumber-1{
+            createButton(x: 100, y: 100, width: 100, height: 50)
+        }
+    }
+    
+    @objc func buttonAction(sender: UIButton!) {
+        print("Button tapped")
     }
 
     override func didReceiveMemoryWarning() {
